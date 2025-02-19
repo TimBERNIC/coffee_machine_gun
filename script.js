499533518743fdcb39c9451a8451bcb9d1a4496b
Tim BERNIC <nicolet.t@hotmail.com>
2025-02-19 19:10:15 +0100

    refactorisation with for integration

--- script.js
+++ script.js
@@ -5,116 +5,97 @@ const monitor = document.getElementById("monitor");
 const launcher = document.getElementById("launcher");
 const mugCase = document.getElementById("mugCase");
 const timer = document.getElementById("timer");
-//Attendre
+
 const wait = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+const pts = ["....", ".", "...", ".."];
 
-//Que l'eau Boue !!!!!
-
+//Que l'eau boue
 async function boiledWater() {
   try {
-    monitor.textContent = "l'eau est en train de chauffer...";
     waterBoil.setAttribute("id", "waterBoilWarm");
     mugCase.setAttribute("id", "mugCaseWithMug");
-
-    timer.textContent = "3";
+    monitor.textContent = "Etape 1/3, chauffage de l'eau";
     await wait(1000);
+    const BoiledMessage = (point) => {
+      monitor.textContent = `l'eau est en train de chauffer ${point}`;
+    };
 
-    monitor.textContent = "l'eau est en train de chauffer.";
-    timer.textContent = "2";
-    await wait(1000);
-
-    monitor.textContent = "l'eau est en train de chauffer..";
-    timer.textContent = "1";
-    await wait(1000);
-
-    monitor.textContent = "Eau chaude!";
-    timer.textContent = "0";
-    await wait(2000);
+    for (let i = 3; i > 0; i--) {
+      BoiledMessage(pts[i]);
+      timer.textContent = i;
+      await wait(1000);
+    }
   } catch (error) {
     console.error("Erreur lors du chauffage de l'eau:", error.message);
     monitor.textContent("Erreur lors du chauffage de l'eau.");
     throw error; //stop le process
   }
 }
-
 //Que le café soit moulu
-
 async function crushCoffee() {
   try {
-    monitor.textContent = "Début de la procédure de moulure...";
+    monitor.textContent = "Etape 2/3, moulure du café";
+    await wait(1000);
+    const crushMessage = (point) => {
+      monitor.textContent = `Début de la procédure de moulure ${point}`;
+    };
 
-    timer.textContent = "3";
-    await wait(1000);
-    monitor.textContent = "Début de la procédure de moulure.";
-    timer.textContent = "2";
-    await wait(1000);
-    monitor.textContent = "Début de la procédure de moulure..";
-    timer.textContent = "1";
-    await wait(1000);
-    monitor.textContent = "Café broyé!";
+    for (let i = 3; i > 0; i--) {
+      crushMessage(pts[i]);
+      timer.textContent = i;
+      await wait(1000);
+    }
     coffeeCase.setAttribute("id", "coffeeCaseEmpty");
-    timer.textContent = "0";
-    await wait(1000);
   } catch (error) {
-    console.error("Erreur lors de la moulure de café:", error.message);
-    monitor.textContent = "Erreur lors de la moulure de café";
+    console.error("Erreur lors de la procédure de moulure:", error.message);
+    monitor.textContent("Erreur lors de la procédure de moulure.");
     throw error; //stop le process
   }
 }
-
 //Coulage du ristretto
-
 async function runningRistretto() {
   try {
+    monitor.textContent = "Etape 3/3, coulage";
     waterBoil.setAttribute("id", "waterBoilEmpty");
-    timer.textContent = "3";
-    monitor.textContent = "café en cours de préparation...";
     await wait(1000);
-    timer.textContent = "2";
-    monitor.textContent = "café en cours de préparation.";
-    await wait(1000);
-    timer.textContent = "1";
-    monitor.textContent = "café en cours de préparation..";
-    await wait(1000);
-    timer.textContent = "0";
-
+    const runningMessage = (point) => {
+      monitor.textContent = `café en cours de préparation ${point}`;
+    };
+    for (let i = 3; i > 0; i--) {
+      runningMessage(pts[i]);
+      timer.textContent = i;
+      await wait(1000);
+    }
     mugCase.setAttribute("id", "mugCaseWithMugFull");
   } catch (error) {
-    console.error("Erreur lors de la préparation:", error.message);
-    monitor.textContent = "Erreur lors de la préparation";
+    console.error("Erreur lors de la préparation :", error.message);
+    monitor.textContent("Erreur lors de la préparation ");
     throw error; //stop le process
   }
 }
-
 // Fonction globale lancement
-
 async function newRistretto() {
   try {
-    monitor.textContent =
-      "Votre commande à bien été prise en compte, veuillez patienter...";
-    await wait(1000);
-    monitor.textContent =
-      "Votre commande à bien été prise en compte, veuillez patienter.";
-    await wait(1000);
-    monitor.textContent =
-      "Votre commande à bien été prise en compte, veuillez patienter..";
-    await wait(1000);
-    monitor.textContent = "Etape 1/3";
-    await wait(2000);
+    const newMessage = (point) => {
+      monitor.textContent = `Votre commande à bien été prise en compte, veuillez patienter ${point}`;
+    };
+
+    for (let i = 3; i > 0; i--) {
+      newMessage(pts[i]);
+      timer.textContent = i;
+      await wait(1000);
+    }
     await boiledWater();
-    monitor.textContent = "Etape 2/3";
-    await wait(2000);
     await crushCoffee();
-    monitor.textContent = "Etape 3/3";
-    await wait(2000);
     await runningRistretto();
+    timer.textContent = 0;
     monitor.textContent = "Votre Café est prêt, veuillez vous servir!";
-  } catch {
-    console.error("Erreur lors de la procédure:", error.message);
-    monitor.textContent = "Erreur lors de la procédure";
-    throw error;
+  } catch (error) {
+    console.error("Erreur lors de la préparation :", error.message);
+    monitor.textContent("Erreur lors de la préparation ");
+    throw error; //stop le process
   }
 }
 
